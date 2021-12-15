a4f1c8829ca4feb428748fd4a78d34b641fd4b94
Sachintha <snishaijth@gmail.com>
2021-12-15 09:34:21 +0530

    Finish day one and basic crud operations

--- 3-crud/src/main/java/lk/ijse/dep7/DeleteDemo.java
+++ 3-crud/src/main/java/lk/ijse/dep7/DeleteDemo.java
@@ -5,19 +5,19 @@ import lk.ijse.dep7.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
-public class DirtyCheckingDemoJPA {
+public class DeleteDemo {
     public static void main(String[] args) {
         try (SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
              Session session = sessionFactory.openSession()) {
 
             session.beginTransaction();
 
-            Student sachintha = session.find(Student.class, 2);
-            System.out.println(sachintha);
-            sachintha.setContactNumber("12345677");
-            System.out.println("Change the phone number :");
-            session.detach(sachintha);
-// methanadi udpdate eka wadinne nehe mokad api detach karapu nisa ethakota dirty checking feture ekat aahuw enne nethi nisa
+            Student student = session.get(Student.class, 1);
+            System.out.println("Is Student inside the context? "+session.contains(student));
+            session.delete(student);
+            System.out.println("Is Student inside the context? "+session.contains(student));
+
+
             session.getTransaction().commit();
         }
     }
---
+++ 3-crud/src/main/java/lk/ijse/dep7/EvictDemo.java
@@ -0,0 +1,22 @@
+package lk.ijse.dep7;
+
+import lk.ijse.dep7.entity.Student;
+import lk.ijse.dep7.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class EvictDemo {
+    public static void main(String[] args) {
+        try (SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+             Session session = sessionFactory.openSession()) {
+
+            Student student = session.find(Student.class, 2);
+            System.out.println(student);
+
+            System.out.println("Is student inside the context :"+ session.contains(student));
+            session.evict(student);
+            System.out.println("Is student inside the context :"+ session.contains(student));
+
+        }
+    }
+}
